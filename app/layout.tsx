fba6b5a498fa5d35c02cd041fd43499b5a0d3673
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-02-17 21:16:15 -0500

    add tailwind & nav, footer, basic content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import Link from 'next/link'
 export default function RootLayout({
   children,
 }: {
@@ -12,7 +12,30 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      
+      <body className='flex flex-col justify-between font-mono h-screen bg-slate-200'>
+        <nav>
+          <ul className='flex'>
+            <li className='p-2'><Link href="/">home</Link></li>
+            <li className='p-2'><Link href="/">about</Link></li>
+            <li className='p-2'><Link href="/">portfolio</Link></li>
+            <li className='p-2'><Link href="/">contact</Link></li>
+          </ul>
+        </nav>
+        <div className='mb-auto'>
+          {children}
+        </div>
+
+        <footer className='flex items-center justify-center text-sm'>
+          <div>
+          {`© ${new Date().getFullYear()} Masroor K`}
+          </div>
+          <ul className='flex '>
+            <li className='p-2'><Link href="/">home</Link></li>
+            <li className='p-2'><Link href="/">contact</Link></li>
+          </ul>
+        </footer>
+      </body>
     </html>
   )
 }
