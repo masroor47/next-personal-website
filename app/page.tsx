d827c8381865fa06df4258ea447ac33a6d9baad8
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-04-01 18:27:42 -0400

    add sample mdx to about me section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 
+
+import AboutSection from './(components)/AboutSection'
+import PortfolioSection from './(components)/PortfolioSection'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <div className='p-4'>
-        <h1 className='text-xl font-semibold'>Welcome to my website!</h1>
-        <p>I will post updates and relevant information on here. Thanks.</p>
+      <div className=''>
+
+        <div className='p-4 my-2'>
+          <h1 className='text-2xl font-semibold'>Welcome to my website!</h1>
+          <p>I will post updates and relevant information on here. Thanks.</p>
+        </div>
+
+        <AboutSection />
+
+        <PortfolioSection />
       </div>
     </main>
   )
