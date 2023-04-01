a70c6918db966620b949be9021c8db1f03b77e89
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-04-01 18:24:18 -0400

    add sections for home page

---
+++ app/(components)/PortfolioSection.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function PortfolioSection() {
+
+  return (
+    <div className="">
+
+      <div className="p-4 my-2">
+        <h1 className="text-xl font-bold">Some of my Projects:</h1>
+      </div>
+
+
+    </div>
+  )
+}
