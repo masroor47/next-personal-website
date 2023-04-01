5c67572e8050916a98e51d332fa22700f00de375
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-04-01 18:38:41 -0400

    clean up About Section

--- app/(components)/AboutSection.tsx
+++ app/(components)/AboutSection.tsx
@@ -1,36 +1,14 @@
-import { getPostData } from "@/lib/posts"
 
 import AboutMe from './aboutMe.mdx';
 
 
-// export async function getStaticProps({ params }: any) {
-//   console.log("IN THE ASYNC FUNC")
-//   const postData = await getPostData("aboutMe");
-  
-
-//   return {
-//     props: {
-//       postData,
-//     }
-//   }
-// }
-
-// async function getData() {
-//   const res = await fetch('../../lib/aboutMe.md')
-// }
-
 export default async function AboutSection({ postData }: any) {
-
-
-  // const aboutMe = await getPostData('aboutMe');
 
   return (
     <div className="p-4 my-2">
-      <h1 className="text-2xl font-bold">About Me:</h1>
+      <h1 className="text-2xl font-bold my-2">About Me:</h1>
 
       <AboutMe />
-
-      {/* <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
 
     </div>
   )
--- app/(components)/PortfolioSection.tsx
+++ app/(components)/PortfolioSection.tsx
@@ -4,13 +4,9 @@
 export default function PortfolioSection() {
 
   return (
-    <div className="">
 
       <div className="p-4 my-2">
-        <h1 className="text-xl font-bold">Some of my Projects:</h1>
+        <h1 className="text-2xl font-bold">Some of my Projects:</h1>
       </div>
-
-
-    </div>
   )
 }
