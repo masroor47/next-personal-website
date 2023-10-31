0d9717d572eacc98dbbaf4f380ee1232d22527a2
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-10-31 02:40:53 -0400

    first site metadata

---
+++ app/data/siteMetadata.ts
@@ -0,0 +1,12 @@
+const siteMetadata = {
+  title: "Masroor K",
+  author: "Masroor K",
+  degrees: [
+    "B.S. Physics",
+    "B.A. Computer Science",
+  ],
+  school: "CUNY Queens College",
+  email: "me@me.com"
+}
+
+export default siteMetadata;
