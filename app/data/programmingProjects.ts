4c27eb6e03edac6ff24475017ec7a5283480080e
Masroor Khonkhodzhaev <masroor47@icloud.com>
2025-05-18 02:01:04 -0400

    added latest projects

--- app/data/programmingProjects.ts
+++ app/data/programmingProjects.ts
@@ -1,5 +1,23 @@
 const programmingProjects = [
   {
+    title: "EDF Drone",
+    img: "/images/projects/edf-drone.jpeg",
+    link: "edf-drone",
+    description: "An electric ducted fan drone with an ESP32 controller that processes sensor data and controls the drone in real-time.",
+  },
+  {
+    title: "Pytorch Clone",
+    img: "/images/projects/quantumgrad-cuda-code.png",
+    link: "pytorch-clone",
+    description: "A from-scratch deep learning library with custom CUDA kernels.",
+  },
+  {
+    title: "Robot Bartender",
+    img: "/images/projects/robot-bartender.png",
+    link: "robot-bartender",
+    description: "A robot bartender that can make cocktails and serve them to customers. Uses Action Chunking Transformer policies to make cocktails according to recipes.",
+  },
+  {
     title: "Solar System Simulator",
     img: "/images/projects/solar-system.png",
     link: "solar-system-simulator",
