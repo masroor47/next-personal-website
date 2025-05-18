c75b315431ca0f9705d8f4c3fc124431fc68d017
Masroor Khonkhodzhaev <masroor47@icloud.com>
2025-05-18 02:02:26 -0400

    add styling to projects list page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,24 +1,26 @@
 import ProjectCard from "../(components)/ProjectCard"
 import programmingProjects from "../data/programmingProjects"
 
-
 export default function ProjectsPage() {
-
-
   return (
-    <div>
+    <div className="w-full min-h-screen flex justify-center">
+      <div className="w-full max-w-6xl px-4 sm:px-6 py-8">
+        <h1 className="text-3xl sm:text-4xl font-bold mb-8 text-center">
+          My Projects
+        </h1>
 
-      <h1>List of all of my projects:</h1>
-
-      {programmingProjects.map((project, id) => (
-        <ProjectCard
-            key={id}
-            title={project.title}
-            img={project.img}
-            link={project.link}
-            description={project.description}
-        />
-      ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 justify-items-center">
+          {programmingProjects.map((project, id) => (
+            <ProjectCard
+              key={id}
+              title={project.title}
+              img={project.img}
+              link={project.link}
+              description={project.description}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
